25b499cd8ed6713e5c0ec8422d82f385bc3d3880
lizengcheng <1986244354@qq.com>
2017-12-27 13:52:03 +0800

    基本完成

--- app/src/main/java/com/example/u/a21/dao/DBOpenHelper.java
+++ app/src/main/java/com/example/u/a21/dao/DBOpenHelper.java
@@ -4,20 +4,23 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by U on 2017/12/26.
- */
+public class DBOpenHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DBNAME = "account.db";
 
-public class DBOpenHelper extends SQLiteOpenHelper{
-    private static final int VERSION=1;                    //定义数据库版本号
-    private static final String DBNAME="account.db";       //定义数据库名
-    public DBOpenHelper(Context context){                  //定义构造函数
-        super(context , DBNAME , null , VERSION);
+    public DBOpenHelper(Context context) {
+
+        super(context, DBNAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table tb_outaccount (_id integer primary key,money decimal,time varchar(10),"
+                + "type varchar(10),address varchar(100),mark varchar(200))");
+        db.execSQL("create table tb_inaccount (_id integer primary key,money decimal,time varchar(10),"
+                + "type varchar(10),handler varchar(100),mark varchar(200))");
+        db.execSQL("create table tb_pwd (password varchar(20))");
+        db.execSQL("create table tb_flag (_id integer primary key,flag varchar(200))");
     }
 
     @Override
